2d51cdacf540974ff687a0e43810beed8bae395b
nourEldehy <49129901+nourEldehy@users.noreply.github.com>
2025-10-20 23:46:09 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 # whatsapp_meta_integration/__init__.py
 from . import controllers
 from . import models
+from . import wizard
--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,19 @@
 # whatsapp_meta_integration/__manifest__.py
 {
     'name': 'WhatsApp Meta Integration',
-    'version': '13.0.1.0.0',
+    'version': '13.0.2.0.0',
     'summary': 'Custom integration with the official WhatsApp Business (Meta) API',
     'author': 'Noureldin ElDehi',
     'website': 'covermatch.com',
     'category': 'Tools',
     'depends': ['base', 'mail'],
     'data': [
+        'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
+        'views/whatsapp_views.xml',
         'views/res_partner_views.xml',
+        'views/res_partner_actions.xml',
+        'wizard/send_whatsapp_wizard_views.xml',
     ],
     'installable': True,
     'application': True,
